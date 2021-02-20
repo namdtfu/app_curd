8001c39506bc28262e65f891d8d428019406d519
minhwalker <minhnh421999@gmail.com>
2021-02-21 00:58:04 +0700

    display data

--- src/admin/Products.js
+++ src/admin/Products.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Wrapper from './Wrapper'
 
 const Products = () => {
+    const [products, setProducts] = useState([])
+
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(async () => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/products')
+
+                const data = await response.json()
+
+                setProducts(data)
+            }
+        )()
+    }, [])
+
     return (
         <Wrapper>
             <div class="table-responsive">
                 <table class="table table-striped table-sm">
                     <thead>
                         <tr>
-                            <th>#</th>
-                            <th>Header</th>
-                            <th>Header</th>
-                            <th>Header</th>
-                            <th>Header</th>
+                            <th>Num</th>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Likes</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1,001</td>
-                            <td>random</td>
-                            <td>data</td>
-                            <td>placeholder</td>
-                            <td>text</td>
-                        </tr>
+                        {products.map(p => {
+                            return (
+                                <tr key={p.id}>
+                                    <td>{p.id}</td>
+                                    <td><img src={p.image} height="180" /></td>
+                                    <td>{p.title}</td>
+                                    <td>{p.likes}</td>
+                                    <td></td>
+                                </tr>
+                            )
+                        })}
+                        
                     </tbody>
                 </table>
             </div>
--- src/admin/Wrapper.js
+++ src/admin/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Menu from '../components/Menu';
-import Nav from '../components/Nav';
+import Menu from './components/Menu';
+import Nav from './components/Nav';
 
 const Wrapper = (props) => {
     return (
